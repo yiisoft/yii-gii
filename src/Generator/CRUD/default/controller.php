57a0adb5798925a8b47ec008fae2e8b43f586fbc
Dmitrii Derepko <xepozz@list.ru>
2023-08-31 15:05:59 +0300

    Add CRUD generator

---
+++ src/Generator/CRUD/default/controller.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+/**
+ * This is the template for generating a controller class file.
+ */
+
+use Yiisoft\Strings\StringHelper;
+
+/* @var $command Yiisoft\Yii\Gii\Generator\CRUD\Command */
+
+$classDefinitionParts = [];
+$classDefinitionParts[] = StringHelper::baseName($command->getControllerClass());
+if (!empty($command->getBaseClass())) {
+    $classDefinitionParts[] = 'extends \\' . trim($command->getBaseClass(), '\\');
+}
+$classDefinition = implode(' ', $classDefinitionParts) . PHP_EOL;
+
+
+echo "<?php\n";
+?>
+
+declare(strict_types=1);
+
+namespace <?= $command->getControllerNamespace() ?>;
+
+use Yiisoft\Yii\View\ViewRenderer;
+
+final class <?= $classDefinition; ?>
+{
+    public function __construct(private ViewRenderer $viewRenderer)
+    {
+        $this->viewRenderer = $viewRenderer->withController($this);
+    }
+<?php foreach ($command->getActions() as $action) : ?>
+
+    public function <?= $action ?>()
+    {
+        return $this->viewRenderer->render('<?= $action ?>');
+    }
+<?php endforeach; ?>
+}
---
+++ src/Generator/CRUD/default/view.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+/**
+ * This is the template for generating an action view file.
+ */
+
+/* @var $command Yiisoft\Yii\Gii\Generator\CRUD\Command */
+/* @var $action string the action ID */
+
+echo "<?php\n";
+?>
+
+declare(strict_types=1);
+
+/**
+ * @var \Yiisoft\View\WebView $this
+ */
+<?= '?>' ?>
+
+<h1><?= $command->getControllerID() . '/' . $action ?></h1>
+
+<p>
+    You may change the content of this page by modifying
+    the file <code><?= '<?=' ?> __FILE__; ?></code>.
+</p>
