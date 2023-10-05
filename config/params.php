76e2982e67e65d6416a6775cf5f0dcba045dcbf5
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-10-05 15:36:32 +0300

    Remove generators from params

--- config/params.php
+++ config/params.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Yiisoft\Yii\Gii\Command\ControllerCommand;
-use Yiisoft\Yii\Gii\Generator as Generators;
 
 return [
     'yiisoft/yii-debug' => [
@@ -24,16 +23,7 @@ return [
     'yiisoft/yii-gii' => [
         'enabled' => true,
         'allowedIPs' => ['127.0.0.1', '::1'],
-        'generators' => [
-            [
-                'class' => Generators\Controller\Generator::class,
-                'parameters' => [],
-            ],
-            [
-                'class' => Generators\ActiveRecord\Generator::class,
-                'parameters' => [],
-            ],
-        ],
+        'generators' => [],
         'parameters' => [
             'templates' => [],
         ],
