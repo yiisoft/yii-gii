c69f4d4a3f885d487f960c2419019ec57d605670
Alexey Rogachev <arogachev90@gmail.com>
2024-09-05 16:47:47 +0500

    Update dependencies

--- src/Validator/NewClassRule.php
+++ src/Validator/NewClassRule.php
@@ -11,11 +11,6 @@ use Yiisoft\Validator\RuleInterface;
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 final class NewClassRule implements RuleInterface
 {
-    public function getName(): string
-    {
-        return 'gii_new_class';
-    }
-
     public function getHandler(): string|RuleHandlerInterface
     {
         return NewClassHandler::class;
