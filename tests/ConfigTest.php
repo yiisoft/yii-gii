cc64b580613a02464e501971eda22d534169c549
samdark <47294+samdark@users.noreply.github.com>
2026-05-26 20:50:50 +0000

    Apply PHP CS Fixer and Rector changes (CI)

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -7,14 +7,14 @@ namespace Yiisoft\Yii\Gii\Tests;
 use Yiisoft\Validator\RuleHandlerResolver\RuleHandlerContainer;
 use Yiisoft\Validator\RuleHandlerResolverInterface;
 
+use function dirname;
+
 final class ConfigTest extends TestCase
 {
     public function testDiDefinitions(): void
     {
         $params = require dirname(__DIR__) . '/config/params.php';
-        $definitions = (static function () use ($params): array {
-            return require dirname(__DIR__) . '/config/di.php';
-        })();
+        $definitions = (static fn(): array => require dirname(__DIR__) . '/config/di.php')();
 
         $this->assertSame(
             RuleHandlerContainer::class,
